efa98436859bb5c7a6c57c9320b8b3b177e09a02
Maxim Samoylov <feardude@gmail.com>
2016-09-24 19:54:36 +0300

    Added setters in Meal (used by JdbcTemplate)

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -6,13 +6,11 @@ import java.time.LocalTime;
 
 public class Meal extends BaseEntity {
 
-    private final LocalDateTime dateTime;
-    private final String description;
-    private final int calories;
+    private LocalDateTime dateTime;
+    private String description;
+    private int calories;
 
-    public Meal() {
-        this(null, null, 0);
-    }
+    public Meal() {}
 
     public Meal(LocalDateTime dateTime, String description, int calories) {
         this(null, dateTime, description, calories);
@@ -39,6 +37,15 @@ public class Meal extends BaseEntity {
     }
     public LocalTime getTime() {
         return dateTime.toLocalTime();
+    }
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public void setCalories(int calories) {
+        this.calories = calories;
     }
 
     @Override
